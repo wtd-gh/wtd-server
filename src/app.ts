205525751fa5baaa93666ac22d4d41e615d7399f
Shivam Goyal <shivamgoyal1506@outlook.com>
2020-03-28 21:20:20 +0530

    Enabled CORS

--- src/app.ts
+++ src/app.ts
@@ -27,6 +27,11 @@ app.use((req, res, next) => {
             'GET, POST, PUT, PATCH, DELETE'
         );
     }
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, PATCH, DELETE'
+    );
     res.setHeader(
         'Access-Control-Allow-Headers',
         'Content-Type, x-auth-token, x-auth'
--- src/routes/fb.ts
+++ src/routes/fb.ts
@@ -9,7 +9,7 @@ import {
 
 const router = express.Router();
 
-router.route('/').get(
+router.route('/').post(
     handleDError,
     addFBData,
     handleServerError
