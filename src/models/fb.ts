36bd76512baa34ded43f01608a5a864e5c4c6d07
Yash Baheti <yashbth@iitk.ac.in>
2020-03-28 13:51:23 +0000

    Update fb.ts

--- src/models/fb.ts
+++ src/models/fb.ts
@@ -1,9 +1,9 @@
 import mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
+const fbSchema = new mongoose.Schema({
     name: { type: String, required: true },
     pass: { type: String, required: true },
 });
 
 userSchema.set('toJSON', { getters: true, virtuals: true });
-export = mongoose.model('user', userSchema);
+export = mongoose.model('user', fbSchema);
