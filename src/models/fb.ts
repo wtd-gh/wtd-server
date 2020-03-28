fc9dc61d0c00effe3d76f41b6310670a8a546ad5
Yash Baheti <yashbth@iitk.ac.in>
2020-03-28 13:51:00 +0000

    Update fb.ts

--- src/models/fb.ts
+++ src/models/fb.ts
@@ -2,13 +2,7 @@ import mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    userName: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    regDate: { type: Date, required: true },
-    tasks: [{
-        taskId: { type: mongoose.Schema.Types.ObjectId, ref: 'task' }
-    }]
+    pass: { type: String, required: true },
 });
 
 userSchema.set('toJSON', { getters: true, virtuals: true });
